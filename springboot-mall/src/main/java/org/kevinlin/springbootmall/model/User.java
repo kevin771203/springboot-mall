d874f6c6d665cce77e0a50c223d4d04be733693a
Kevin <b0952193319@gmail.com>
2025-04-21 11:12:33 +0800

    Add email validation and user existence check

--- springboot-mall/src/main/java/org/kevinlin/springbootmall/model/User.java
+++ springboot-mall/src/main/java/org/kevinlin/springbootmall/model/User.java
@@ -1,10 +1,17 @@
 package org.kevinlin.springbootmall.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.Date;
 
 public class User {
     private Integer userId;
+
+    @JsonProperty("e_mail")
     private String email;
+
+    @JsonIgnore
     private String password;
     private Date createdDate;
     private Date lastModifiedDate;
--- springboot-mall/src/main/java/org/kevinlin/springbootmall/service/impl/UserServiceImpl.java
+++ springboot-mall/src/main/java/org/kevinlin/springbootmall/service/impl/UserServiceImpl.java
@@ -4,17 +4,33 @@ import org.kevinlin.springbootmall.dao.UserDao;
 import org.kevinlin.springbootmall.dto.UserRegisterRequest;
 import org.kevinlin.springbootmall.model.User;
 import org.kevinlin.springbootmall.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
 
 @Component
 public class UserServiceImpl implements UserService {
+
+    private final static Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
 
     @Autowired
     private UserDao userDao;
 
     @Override
     public Integer register(UserRegisterRequest userRegisterRequest) {
+
+        //檢查 email 是否存在
+        User user = userDao.getUserEmailByEmail(userRegisterRequest.getEmail());
+
+        if (user != null) {
+            log.warn("User with email {} already exists", userRegisterRequest.getEmail());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        //創建 user
         return userDao.createUser(userRegisterRequest);
     }
 
