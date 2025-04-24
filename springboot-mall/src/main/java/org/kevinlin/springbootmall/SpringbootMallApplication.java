ffca23ddd843a46b7b2c9dd6f12dabcbdb898acb
Kevin <b0952193319@gmail.com>
2025-04-24 21:13:19 +0800

    Add environment configuration and CI/CD workflows

--- springboot-mall/src/main/java/org/kevinlin/springbootmall/SpringbootMallApplication.java
+++ springboot-mall/src/main/java/org/kevinlin/springbootmall/SpringbootMallApplication.java
@@ -1,10 +1,13 @@
 package org.kevinlin.springbootmall;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootMallApplication {
+
+    Dotenv dotenv = Dotenv.load();
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootMallApplication.class, args);
