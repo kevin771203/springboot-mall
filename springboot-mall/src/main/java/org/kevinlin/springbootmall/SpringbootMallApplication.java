b636a34282168451aa0f71475a43412f416acb86
Kevin <b0952193319@gmail.com>
2025-04-25 02:53:05 +0800

    Update deployment workflow and remove dotenv initialization

--- springboot-mall/src/main/java/org/kevinlin/springbootmall/SpringbootMallApplication.java
+++ springboot-mall/src/main/java/org/kevinlin/springbootmall/SpringbootMallApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootMallApplication {
 
-    Dotenv dotenv = Dotenv.load();
-
     public static void main(String[] args) {
         SpringApplication.run(SpringbootMallApplication.class, args);
     }
